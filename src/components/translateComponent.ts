2c5e8ef4e91839e9ee9fdeb251324c1d6e68a51c
SimonKindhauser <simon.kindhauser@gmx.ch>
2016-12-28 17:56:38 +0100

    adds constructor to translate component

--- src/components/translateComponent.ts
+++ src/components/translateComponent.ts
@@ -4,10 +4,23 @@ namespace sczGeb
 {
   export class TranslateComponent extends sczEcs.Component
   {
-    public x: number = 0;
-    public y: number = 0;
-    public z: number = 0;
-    public angle: number = 0;
-    public rotationPoint = {x:0, y:0};
+    public x: number;
+    public y: number;
+    public z: number;
+    public angle: number;
+    public rotationPoint: {x: number, y: number};
+
+    public constructor(
+      x: number = 0, y: number = 0,
+      z: number = 0, angle: number = 0,
+      rotationPoint = {x:0, y:0})
+    {
+      super();
+      this.x = x;
+      this.y = y;
+      this.z = z;
+      this.angle = angle;
+      this.rotationPoint = rotationPoint;
+    }
   }
 }
