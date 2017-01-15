95eaf73724b19fbc2f15e77b1d8d90d3de587ae7
SimonKindhauser <simon.kindhauser@bitmessage.ch>
2017-01-15 14:44:34 +0100

    merges coordinates into position object

--- src/components/translateComponent.ts
+++ src/components/translateComponent.ts
@@ -4,21 +4,17 @@ namespace sczGeb
 {
   export class TranslateComponent extends sczEcs.Component
   {
-    public x: number;
-    public y: number;
-    public z: number;
+    public position: {x: number, y: number, z: number};
     public angle: number;
     public rotationPoint: {x: number, y: number};
 
     public constructor(
-      x: number = 0, y: number = 0,
-      z: number = 0, angle: number = 0,
-      rotationPoint = {x:0, y:0})
+      position: {x: number, y: number, z: number} = {x: 0, y: 0, z: 0},
+      angle: number = 0,
+      rotationPoint: {x: number, y: number} = {x: 0, y: 0})
     {
       super();
-      this.x = x;
-      this.y = y;
-      this.z = z;
+      this.position = position;
       this.angle = angle;
       this.rotationPoint = rotationPoint;
     }
