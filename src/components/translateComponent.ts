9416c28df4faaff18a7487d8127e9d0cde571769
SimonKindhauser <simon.kindhauser@bitmessage.ch>
2017-02-01 12:32:26 +0100

    adds offset to translate component

--- src/components/translateComponent.ts
+++ src/components/translateComponent.ts
@@ -6,17 +6,17 @@ namespace sczGeb
   {
     public position: {x: number, y: number, z: number};
     public angle: number;
-    public rotationPoint: {x: number, y: number};
+    public offset: {x: number, y: number};
 
     public constructor(
       position: {x: number, y: number, z: number} = {x: 0, y: 0, z: 0},
       angle: number = 0,
-      rotationPoint: {x: number, y: number} = {x: 0, y: 0})
+      offset: {x: number, y: number} = {x: 0, y: 0})
     {
       super();
       this.position = position;
       this.angle = angle;
-      this.rotationPoint = rotationPoint;
+      this.offset = offset;
     }
   }
 }
