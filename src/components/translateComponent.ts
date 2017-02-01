30a937468d5892db08df504bf369e7373b626e9e
SimonKindhauser <simon.kindhauser@bitmessage.ch>
2017-02-01 13:56:28 +0100

    adds parent to translate component

--- src/components/translateComponent.ts
+++ src/components/translateComponent.ts
@@ -8,18 +8,37 @@ namespace sczGeb
     public angle: number;
     public offset: {x: number, y: number};
     public size: {x: number, y: number};
+    public parent: TranslateComponent;
 
     public constructor(
       position: {x: number, y: number, z: number} = {x: 0, y: 0, z: 0},
       angle: number = 0,
       offset: {x: number, y: number} = {x: 0, y: 0},
-      size: {x: number, y: number} = null)
+      size: {x: number, y: number} = null,
+      parent: TranslateComponent = null)
     {
       super();
       this.position = position;
       this.angle = angle;
       this.offset = offset;
       this.size = size;
+      this.parent = parent;
+    }
+
+    static getPositionOf(translateComponent: TranslateComponent):
+      {x: number, y: number, z: number}
+    {
+      var result: {x: number, y: number, z: number} = {
+        x: translateComponent.position.x,
+        y: translateComponent.position.y,
+        z: translateComponent.position.z
+      };
+      while((translateComponent = translateComponent.parent) != null){
+        result.x += translateComponent.position.x;
+        result.y += translateComponent.position.y;
+        result.z += translateComponent.position.z;
+      }
+      return result;
     }
   }
 }
